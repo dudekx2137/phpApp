2495aef7b8ad3077c1ce17307b7f4338ac868968
dudekx2137 <97676803+dudekx2137@users.noreply.github.com>
2023-01-22 12:53:28 +0100

    Add files via upload

--- register.php
+++ register.php
@@ -35,35 +35,7 @@
         </fieldset>
     </form>
     <?php
-        session_start();
-        require_once"connect.php";
-        error_reporting(E_ALL ^ E_NOTICE);  
-        $conn = @mysqli_connect($host, $db_user, $db_password, $db_name);
-
-        if (mysqli_connect_errno()!=0)
-        {
-            echo "Debug: ".mysqli_connect_errno();
-        }else{
-            $login = $_POST['username'];
-            $password = $_POST['password'];
-            $password2 = $_POST['password2'];
-            $email = $_POST['email'];
-            $queryCheck = "SELECT * FROM users WHERE login = '$login' OR email = '$email'";
-            $result = mysqli_query($conn, $queryCheck);
-            $row = mysqli_fetch_assoc($result);
-            if($row['login'] == $login || $row['email'] == $email){
-                echo "<h2>Istnieje użytkownik z takim samym loginem badz mailem!</h2>";
-            }else{
-                if($password == $password2){
-                    $userInsertQuery = "INSERT INTO users (login, password, email) Values('$login','$password','$email')";
-                    $result = mysqli_query($conn, $userInsertQuery);
-                    echo "<h2>Konto dodano pomyślnie</h2>";
-                }else{
-                    echo "<h2>Podane hasła nie są takie same!</h2>";
-                }
-            }
-            mysqli_close($conn);
-        }
+        include'register_form.php';
     ?>
 </body>
 </html>
